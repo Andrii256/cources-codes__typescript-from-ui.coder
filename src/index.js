bdb33bc969a067cf2753e6284ccf863f83257f4a
Andrii Hrushetskyi <andrii.hrushetskii@gmail.com>
2021-02-13 22:34:46 -0500

    practice interfaces

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 "use strict";
-var apple = {
-    name: 'Apple',
-    color: 'red'
-};
-apple.nutrient1 = '50mg';
-apple.nut2 = '20mg';
-apple[325] = 'fuck';
-// apple[true] = 'yes';
-console.log(apple);
